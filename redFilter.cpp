638f64d147baa064eb8e2748a5b9a2715efa6fc4
DanilPestryakov <55560885+DanilPestryakov@users.noreply.github.com>
2019-12-25 22:28:33 +0300

    Update redFilter.cpp

--- redFilter.cpp
+++ redFilter.cpp
@@ -5,34 +5,14 @@ void redFilter::applyFilter(image_data imgData) {
 	int newLineSize;
 	int pos;
 	int lineSize;
-	int newW1;
-	int newW2;
-	int newH1;
-	int newH2;
-	if (u != 0){
+	if (u != 0)
 		newW = imgData.w / b - imgData.w / u - 1;
-		newW1 = imgData.w / u;
-		newW2 = imgData.w / b;
-	}
-	else{
-		
+	else
 		newW = imgData.w / b - 1;
-		newW1 = imgData.w / u;
-		newW2 = imgData.w / b;
-	}
 	if (l != 0)
-	{
-			newH = imgData.h / r - imgData.h / l - 1;
-		newH1 = imgData.h / l;
-		newH2 = imgData.h / r;
-	
-	}
-	else{
-			newH = imgData.h / r - 1;
-		newH1 = imgData.h / l;
-		newH2 = imgData.h / r;
-	
-	}
+		newH = imgData.h / r - imgData.h / l - 1;
+	else
+		newH = imgData.h / r - 1;
 	newLineSize = imgData.compPerPixel * newW;
 	lineSize = imgData.compPerPixel * imgData.w;
 	if (l != 0 && u != 0)
@@ -43,13 +23,13 @@ void redFilter::applyFilter(image_data imgData) {
 		pos = lineSize * (imgData.h / l) + imgData.compPerPixel;
 	if (l == 0 && u == 0)
 		pos = 0;
-	for (int i = newW1; i < newW2; i++)
+	for (int i = 0; i <= newH; i++)
 	{
-		for (int j = newH1; j < newH2; j++)
+		for (int j = 0; j <= newLineSize; j += imgData.compPerPixel)
 		{
-			imgData.pixels[(imgData.w * i + j) * imgData.compPerPixel] = (unsigned char)255;
-			imgData.pixels[(imgData.w * i + j) * imgData.compPerPixel + 1] = (unsigned char)0;
-			imgData.pixels[(imgData.w * i + j) * imgData.compPerPixel + 2] = (unsigned char)0;
+			imgData.pixels[pos + (lineSize)* i + j] = (unsigned char)255;
+			imgData.pixels[pos + (lineSize)* i + j + 1] = (unsigned char)0;
+			imgData.pixels[pos + (lineSize)* i + j + 2] = (unsigned char)0;
 		}
 	}
 }
