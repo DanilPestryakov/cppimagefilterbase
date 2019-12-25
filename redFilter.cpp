862abe6c92f4952817d261e731c5ffaefd2aff8c
DanilPestryakov <55560885+DanilPestryakov@users.noreply.github.com>
2019-12-25 23:16:46 +0300

    Update redFilter.cpp

--- redFilter.cpp
+++ redFilter.cpp
@@ -5,21 +5,21 @@ void redFilter::applyFilter(image_data imgData) {
 	int newH1;
 	int newH2;
 	if (u != 0){
-		newW1 = imgData.w / u;
-		newW2 = imgData.w / b;
-	}
-	else{
-		newW1 = 0;
-		newW2 = imgData.w / b;
-	}
-	if (l != 0)
-	{
-		newH1 = imgData.h / l;
-		newH2 = imgData.h / r;
+		newH1 = imgData.h / u;
+		newH2 = imgData.h / b;
 	}
 	else{
 		newH1 = 0;
-		newH2 = imgData.h / r;
+		newH2 = imgData.h / b;
+	}
+	if (l != 0)
+	{
+		newW1 = imgData.w / l;
+		newW2 = imgData.w / r;
+	}
+	else{
+		newW1 = 0;
+		newW2 = imgData.w / r;
 	}
 
 	for (int i = newW1; i < newW2; i++)
