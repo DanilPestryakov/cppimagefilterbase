f170e17716a21381821628c31fa50d1852f04ada
DanilPestryakov <55560885+DanilPestryakov@users.noreply.github.com>
2020-01-15 00:39:42 +0300

    15.01

--- redFilter.cpp
+++ redFilter.cpp
@@ -1,34 +1,30 @@
 #include "redFilter.h"
 void redFilter::applyFilter(image_data imgData) {
-	int newW1;
-	int newW2;
-	int newH1;
-	int newH2;
-	if (u != 0){
-		newH1 = imgData.h / u;
-		newH2 = imgData.h / b;
-	}
-	else{
-		newH1 = 0;
-		newH2 = imgData.h / b;
-	}
+	int newCoord[4];
+	if (u != 0)
+		newCoord[0] = imgData.h / u;
+	else
+		newCoord[0] = 0;
 	if (l != 0)
-	{
-		newW1 = imgData.w / l;
-		newW2 = imgData.w / r;
-	}
-	else{
-		newW1 = 0;
-		newW2 = imgData.w / r;
-	}
+		newCoord[1] = imgData.w / l;
+	else
+		newCoord[1] = 0;
+	if (b != 0)
+		newCoord[2] = imgData.h / b;
+	else
+		newCoord[2] = 0;
+	if (r != 0)
+		newCoord[3] = imgData.w / r;
+	else
+		newCoord[3] = 0;
 
-	for (int i = newH1; i < newH2; i++)
+	for (int i = newCoord[0]; i < newCoord[2]; i++)
 	{
-		for (int j = newW1; j < newW2; j++)
+		for (int j = newCoord[1]; j < newCoord[3]; j++)
 		{
-			imgData.pixels[(imgData.w * i + j) * imgData.compPerPixel] = (unsigned char)255;
-			imgData.pixels[(imgData.w * i + j) * imgData.compPerPixel + 1] = (unsigned char)0;
-			imgData.pixels[(imgData.w * i + j) * imgData.compPerPixel + 2] = (unsigned char)0;
+			imgData.pixels[(i * imgData.w + j) * imgData.compPerPixel] = (unsigned char)255;
+			imgData.pixels[(i * imgData.w + j) * imgData.compPerPixel + 1] = (unsigned char)0;
+			imgData.pixels[(i * imgData.w + j) * imgData.compPerPixel + 2] = (unsigned char)0;
 		}
 	}
 }
