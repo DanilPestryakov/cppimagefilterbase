e1d7e456b37a3afb6c39ae89959b39b434a9c10c
DanilPestryakov <55560885+DanilPestryakov@users.noreply.github.com>
2020-01-15 01:34:32 +0300

    15.01 1:34

--- parser.cpp
+++ parser.cpp
@@ -3,44 +3,25 @@
 void parser::pars(char* filename)
 {
 	std::ifstream in(filename);
-	std::string nameOfFilter;
-	cParams str;
-	while (in) {
-		std::getline(in, nameOfFilter, ' ');
-		if (nameOfFilter.find("Red") != -1) {
-			str.nameOfFilter = "Red";
-			in >> str.u;
-			in >> str.l;
-			in >> str.b;
-			in >> str.r;
-			filterQ.push_back(str);
+	std::string line;
+	int counter = 0;
+	while (getline(in, line)) {
+		cParams Str;
+		std::istringstream i(line);
+		for (std::string str; i >> str;) {
+			if (counter == 0)
+				Str.nameOfFilter = str;
+			if (counter == 1)
+				Str.u = (stoi(str));
+			if (counter == 2)
+				Str.l = (stoi(str));
+			if (counter == 3)
+				Str.b = (stoi(str));
+			if (counter == 4)
+				Str.r = (stoi(str));
+			counter++;
 		}
-		if (nameOfFilter.find("Treshold") != -1) {
-			str.nameOfFilter = "Treshold";
-			in >> str.u;
-			in >> str.l;
-			in >> str.b;
-			in >> str.r;
-			filterQ.push_back(str);
-		}
-		
-		if (nameOfFilter.find("Edge") != -1) {
-			str.nameOfFilter = "Edge";
-			in >> str.u;
-			in >> str.l;
-			in >> str.b;
-			in >> str.r;
-			filterQ.push_back(str);
-		}
-		if (nameOfFilter.find("Blur") != -1) {
-			str.nameOfFilter = "Blur";
-			in >> str.u;
-			in >> str.l;
-			in >> str.b;
-			in >> str.r;
-			filterQ.push_back(str);
-		}
+		filterQ.push_back(Str);
+		counter = 0;
 	}
 }
-
-
